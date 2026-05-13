4d9de330bc1b90f82d4f5cc5e1f710769375038e
instadotanalytics <instadotanalytics@gmail.com>
2026-05-13 12:11:08 +0530

    database

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS (Fixed - added all ports)
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: ['https://intechzone.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
